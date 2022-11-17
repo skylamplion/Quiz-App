79f680cc3cdaa4344bbc400720c90407f9fa313d
GAGAN DEEP SINGH <108108276+skylamplion@users.noreply.github.com>
2022-11-17 23:21:14 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is correct about JavaScript?",
+    answer: "JavaScript is an Object-Based language",
+    options: [
+      "JavaScript is an Object-Based language",
+      "JavaScript is Assembly-language",
+      "JavaScript is an Object-Oriented language",
+      "JavaScript is a High-level language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is not javascript data types?",
+    answer: "All of the mentioned",
+    options: [
+      "Null type",
+      "Undefined type",
+      "Number type",
+      "All of the mentioned"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following can be used to call a JavaScript Code Snippet?",
+    answer: "Function/Method",
+    options: [
+      "Function/Method",
+      "Preprocessor",
+      "Triggering Event",
+      "RMI"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following scoping type does JavaScript use?",
+    answer: "Lexical",
+    options: [
+      "Sequential",
+      "Segmental",
+      "Lexical",
+      "Literal"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Why JavaScript Engine is needed?",
+    answer: "Interpreting the JavaScript",
+    options: [
+      "Both Compiling & Interpreting the JavaScript",
+      "Parsing the javascript",
+      "Interpreting the JavaScript",
+      "Compiling the JavaScript"
+    ]
+  }
+
+];
