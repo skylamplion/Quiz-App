482d9939ea34974bc38a57a42b9f5334034269c9
GAGAN DEEP SINGH <108108276+skylamplion@users.noreply.github.com>
2023-06-07 17:56:15 +0530

    Add files via upload

---
+++ Quiz Application with Timer/js/css.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions2 = [
+    {
+    numb: 1,
+    question: "The property in CSS used to change the background color of an element is",
+    answer: "background-color",
+    options: [
+      "bgcolor",
+      "color",
+      "background-color",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The property in CSS used to change the text color of an element is -",
+    answer: "color",
+    options: [
+      "bgcolor",
+      "color",
+      "background-color",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The CSS property used to control the element's font size is -",
+    answer: "font-size",
+    options: [
+      "text-style",
+      "text-size",
+      "font-size",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The HTML attribute used to define the inline styles is -",
+    answer: "style",
+    options: [
+      "style",
+      "styles",
+      "class",
+      "None of the above"
+    ]
+  }
+];
